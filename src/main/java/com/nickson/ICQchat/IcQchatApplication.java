74e005b50372781cb9ef643a2364d7bbce3db907
nikolajred <nikolajred@gmail.com>
2020-06-15 19:56:34 +0300

    Initial commit

---
+++ src/main/java/com/nickson/ICQchat/IcQchatApplication.java
@@ -0,0 +1,13 @@
+package com.nickson.ICQchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcQchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcQchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickson/ICQchat/IcQchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickson.ICQchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcQchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
